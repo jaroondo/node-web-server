19ac822b938b8e6e18ac9b5ebe05a96604ae82ac
Jaroslav Ondo <jaroslav.ondo@tik.local>
2018-11-07 13:20:39 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => { // middleware
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        };
+    });
+    next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+app.listen(5500, '127.0.0.1', () => {
+    console.log('Server is running. "Go Live" on 127.0.0.1:5500 in browser.');
+});
